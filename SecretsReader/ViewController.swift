5d58d4178e96337e11cd1861ac4b5a85226e13a0
Alex Nagy <therebeloper@gmail.com>
2020-09-16 12:58:00 +0300

    first commit

--- SecretsReader/ViewController.swift
+++ SecretsReader/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Secrets
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let manager = SecretsManager(key: "12345")
+        if let secret = manager.readSecret() {
+            print("The secret is: \(secret)")
+        } else {
+            print("Failed to read secret")
+        }
     }
 
 
